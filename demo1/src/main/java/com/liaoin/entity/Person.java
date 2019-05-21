d4c4ed70c31ce5187a562755fe5729c0a8e0baff
gumuming <40025442+gumuming@users.noreply.github.com>
2019-05-21 21:48:08 +0800

    Update Person.java

--- demo1/src/main/java/com/liaoin/entity/Person.java
+++ demo1/src/main/java/com/liaoin/entity/Person.java
@@ -8,9 +8,30 @@ import java.io.Serializable;
  * @Author: xiang_chat
  * @Date: 2019/5/21 10:28
  */
-@Data
-public class Person implements Serializable {
-    private String name;
-    private String age;
-    private String sex;
+ @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "t_user")
+public class User {
+
+    @Id
+    @KeySql(genId = UUIdGenId.class)
+    private String id;
+
+    @Column(name = "create_at",insertable = false,updatable = false)
+    private Date createAt;
+
+    @Column(name = "update_at",insertable = false,updatable = false)
+    private Date updateAt;
+
+    @Column(name = "c_user_name")
+    private String username;
+
+    @Column(name = "c_phone_num")
+    private String phoneNum;
+
+
 }
